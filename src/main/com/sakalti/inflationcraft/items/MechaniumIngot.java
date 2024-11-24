7b475d11396ec1804bc7d33209a1d0be39070174
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-11-24 14:26:18 +0900

    Create MechaniumIngot.java

---
+++ src/main/com/sakalti/inflationcraft/items/MechaniumIngot.java
@@ -0,0 +1,12 @@
+package com.sakalti.inflationcraft.items;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+public class MechaniumIngot extends Item {
+    public MechaniumIngot() {
+        super(new Item.Properties().group(ItemGroup.MATERIALS));
+    }
+}
