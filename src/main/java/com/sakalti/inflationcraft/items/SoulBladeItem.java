c81578411217abfa502a93f77e82e1407765125b
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-11-28 18:13:05 +0900

    Create SoulBladeItem.java

---
+++ src/main/java/com/sakalti/inflationcraft/items/SoulBladeItem.java
@@ -0,0 +1,44 @@
+package com.sakalti.inflationcraft.item;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraftforge.common.ToolType;
+
+public class SoulBlade extends SwordItem {
+
+    public SoulBlade() {
+        super(new IItemTier() {
+            @Override
+            public int getUses() {
+                return 2000; // 耐久値
+            }
+
+            @Override
+            public float getSpeed() {
+                return 1.7f; // 攻撃速度
+            }
+
+            @Override
+            public float getAttackDamageBonus() {
+                return 14.5f; // 攻撃力
+            }
+
+            @Override
+            public int getLevel() {
+                return 4; // ツールのレベル（ダイヤ以上）
+            }
+
+            @Override
+            public int getEnchantmentValue() {
+                return 13; // エンチャントの付与確率
+            }
+
+            @Override
+            public Ingredient getRepairIngredient() {
+                return Ingredient.of(Registration.SOUL_SAND.get()); // 修理材料（ソウルサンド）
+            }
+        }, 3, -2.4f, new Item.Properties().tab(InflationCraft.TAB).durability(2000).addToolType(ToolType.SWORD, 3));
+    }
+}
